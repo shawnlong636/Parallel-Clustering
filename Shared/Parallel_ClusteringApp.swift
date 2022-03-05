b8b58ad4537fc9ab81834fa1b3318d62a8a06bdb
Shawn Long <slong024@ucr.edu>
2022-03-05 12:02:21 -0800

    Initial Commit

---
+++ Shared/Parallel_ClusteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parallel_ClusteringApp.swift
+//  Shared
+//
+//  Created by Shawn Long on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Parallel_ClusteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
